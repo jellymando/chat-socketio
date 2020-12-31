db033d7412770c779cdad1fb6fcdabf2ba5b9799
dain <cicikdi@naver.com>
2020-12-31 16:13:53 +0900

    fix: public 경로 수정

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html');
